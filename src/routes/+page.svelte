1809c38596ef88ace035e83089fd54042dcabcd9
Matej Bačo <matejbaco2000@gmail.com>
2024-12-02 10:02:33 +0100

    Init SvelteKit + shadcn-svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="mx-auto w-full max-w-xl mt-6">
+    <h1 class="mb-8">Test button</h1>
+    <Button>Click me</Button>
+</div>

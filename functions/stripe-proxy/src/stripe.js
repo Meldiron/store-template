e0dae7cf1c52b732e77d66e81169546ad837f421
loks0n <22452787+loks0n@users.noreply.github.com>
2024-12-04 11:31:47 +0000

    chore: remove webhook

--- functions/stripe-proxy/src/stripe.js
+++ functions/stripe-proxy/src/stripe.js
@@ -11,56 +11,18 @@ class StripeService {
   }
 
   /**
-   * @param {string} userId
+   * @param {import('stripe').stripe.Checkout.SessionCreateParams.LineItem[]} lineItems
    * @param {string} successUrl
    * @param {string} failureUrl
    */
-  async checkoutPayment(context, userId, successUrl, failureUrl) {
-    /** @type {import('stripe').Stripe.Checkout.SessionCreateParams.LineItem} */
-    const lineItem = {
-      price_data: {
-        unit_amount: 1000, // $10.00
-        currency: 'usd',
-        product_data: {
-          name: 'Product',
-        },
-      },
-      quantity: 1,
-    };
-
-    try {
-      return await this.client.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [lineItem],
-        success_url: successUrl,
-        cancel_url: failureUrl,
-        client_reference_id: userId,
-        metadata: {
-          userId,
-        },
-        mode: 'payment',
-      });
-    } catch (err) {
-      context.error(err);
-      return null;
-    }
-  }
-
-  /**
-   * @returns {import("stripe").Stripe.DiscriminatedEvent | null}
-   */
-  validateWebhook(context, req) {
-    try {
-      const event = this.client.webhooks.constructEvent(
-        req.bodyBinary,
-        req.headers['stripe-signature'],
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-      return /** @type {import("stripe").Stripe.DiscriminatedEvent} */ (event);
-    } catch (err) {
-      context.error(err);
-      return null;
-    }
+  async createSession(lineItems, successUrl, failureUrl) {
+    return await this.client.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      success_url: successUrl,
+      cancel_url: failureUrl,
+      mode: 'payment',
+    });
   }
 }
 

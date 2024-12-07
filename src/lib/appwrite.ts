eb5206e59a223022fc8613d3ab0b1e229ad81291
Matej Bačo <matejbaco2000@gmail.com>
2024-12-07 15:13:07 +0100

    Add more services

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,8 @@
-import { Client, Account } from 'appwrite';
+import { Client, Account, Databases, Functions, Storage } from 'appwrite';
 
 const client = new Client().setEndpoint('https://qa17.appwrite.org/v1').setProject('store');
 
 export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const functions = new Functions(client);

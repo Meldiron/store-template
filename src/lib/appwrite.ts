5f77261d8ad3fa91b66cf929d960b0b2976ff5af
loks0n <22452787+loks0n@users.noreply.github.com>
2024-12-07 14:12:11 +0000

    chore: env vars

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,11 @@
+import {
+	PUBLIC_APPWRITE_SITE_API_ENDPOINT,
+	PUBLIC_APPWRITE_SITE_PROJECT_ID
+} from '$env/static/public';
 import { Client, Account } from 'appwrite';
 
-const client = new Client().setEndpoint('https://qa17.appwrite.org/v1').setProject('store');
+const client = new Client()
+	.setEndpoint(PUBLIC_APPWRITE_SITE_API_ENDPOINT)
+	.setProject(PUBLIC_APPWRITE_SITE_PROJECT_ID);
 
 export const account = new Account(client);
